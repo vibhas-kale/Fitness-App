5add37aa4cc3c2ede438adcacf0ed8baff342968
arpitklin <arpitladole4@gmail.com>
2025-06-27 19:20:58 +0530

    Initial project commit.

---
+++ src/test/java/com/fit/app/Fitness/App/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.app.Fitness.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
